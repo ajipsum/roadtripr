f222338acfc4b959cc8ca68b6889bfc1891f4f2c
Magana <josemaganadev@gmail.com>
2018-02-09 16:21:46 -0600

    added github controller

---
+++ roadtripr/app/controllers/githubcontroller.py
@@ -0,0 +1,42 @@
+from github3 import login
+import configparser
+
+
+class GithubController:
+    '''
+    '''
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
+        gh = login(token=config['GITHUB']['ACCESS_TOKEN'])
+        self.repo = gh.repository('Magana', 'roadtripr')
+
+
+    def get_issue_counts(self):
+        issue_counts = dict()
+        issues = self.repo.issues()
+
+        for i in issues:
+            user = str(i.user)
+            if user in issue_counts:
+                issue_counts[user] += 1
+            else:
+                issue_counts[user] = 1
+
+        return issue_counts
+
+
+    def get_commit_counts(self):
+        commit_counts = dict()
+        commits = self.repo.commits()
+
+        for c in commits:
+            user = str(c.author)
+            if user in commit_counts:
+                commit_counts[user] += 1
+            else:
+                commit_counts[user] = 1
+
+        return commit_counts
+
