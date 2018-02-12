843fa459f1e90abb014f1a80ef7939bb96e1e3a9
huangyvonnee <huangyvonnee@gmail.com>
2018-02-12 16:58:37 -0600

    pull index.html from react into flask app

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def send(path):
+	return send_from_directory('roadtripr/public', 'index.html')
+""" 
+    if(path == ""):
+        return send_from_directory('roadtripr/public', 'index.html')
+    else:
+        if(os.path.exists("roadtripr/public/" + path)):
+            return send_from_directory('roadtripr/public', path)
+        else:
+            return send_from_directory('roadtripr/public', 'index.html')
+"""
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(use_reloader=True, threaded=True)
