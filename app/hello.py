c1f9d1238ce71e4e144e8fb480f90a78b7837a2d
jonbrew <jonob03@yahoo.com>
2018-02-12 19:43:27 -0600

    Flask serving static files fix

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def send(path):
-	return send_from_directory('roadtripr/public', 'index.html')
+	return render_template('index.html')
 """ 
     if(path == ""):
         return send_from_directory('roadtripr/public', 'index.html')
