b62e0c2e557253ff22ae53d54318a331d73d50af
Magana <josemaganadev@gmail.com>
2018-02-14 17:45:02 -0600

    about gets github commit and issue data

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, jsonify
+from controllers.githubcontroller import GithubController
 app = Flask(__name__)
 
+github_controller = None
+
+@app.route('/about', methods=['GET'])
+def github_contribs():
+    global github_controller
+    
+    if github_controller == None:
+        github_controller = GithubController()
+    
+    result = {'commits': github_controller.get_commit_counts(),
+              'issues': github_controller.get_issue_counts()}
+    return render_template('about.html', **result)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
