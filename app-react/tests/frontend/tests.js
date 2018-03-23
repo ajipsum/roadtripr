51a8976595d57df698d7e723d2bf245086a5c357
huangyvonnee <huangyvonnee@gmail.com>
2018-03-22 21:51:36 -0500

    added more unit tests

---
+++ app-react/tests/frontend/tests.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import {expect} from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import About from '..src/templates/about.js'
+import Cities from '../src/templates/cities.js'
+import City from '../src/templates/city.js'
+import Parks from '../src/templates/parks.js'
+//import Park from '../src/templates/park.js'
+import Restaurants from '../src/templates/restaurants.js'
+//import Restaurant from '../src/templates/restaurant.js'
+
+describe('Test About Page', () => {
+    before(function() {
+      this.jsdom = require('jsdom-global')();
+    })
+
+    after(function() {
+      this.jsdom();
+    })
+
+    const wrapper = shallow(<About />);
+    it('exists', () => {
+      expect(wrapper.find('#about').exists()).to.eql(true);
+    });
+});
+
+describe('Test Cities Page', () => {
+    before(function() {
+      this.jsdom = require('jsdom-global')();
+    })
+
+    after(function() {
+      this.jsdom();
+    })
+
+    const wrapper = shallow(<Cities />);
+    it('exists', () => {
+      expect(wrapper.find('#cities').exists()).to.eql(true);
+    });
+});
+
+describe('Test Parks Page', () => {
+    before(function() {
+      this.jsdom = require('jsdom-global')();
+    })
+
+    after(function() {
+      this.jsdom();
+    })
+
+    const wrapper = shallow(<Parks />);
+    it('exists', () => {
+      expect(wrapper.find('#parks').exists()).to.eql(true);
+    });
+});
+
+describe('Test Restaurants Page', () => {
+    before(function() {
+      this.jsdom = require('jsdom-global')();
+    })
+
+    after(function() {
+      this.jsdom();
+    })
+
+    const wrapper = shallow(<Restaurants />);
+    it('exists', () => {
+      expect(wrapper.find('#restaurants').exists()).to.eql(true);
+    });
+});
+
+// Testing Instance
+describe("Test Park", function() {
+
+  before(function() {
+    this.jsdom = require('jsdom-global')();
+  })
+
+  after(function() {
+    this.jsdom();
+  })
+
+  it('Test park instance for website', function () {
+    const wrapper = mount(<Park restaurants={{state: {restaurants: ['McDonalds']}}} />);
+    expect(wrapper.state().restaurants).contains({restaurants: ['McDonalds']});
+  });
+});
---
+++ app-react/tests/frontend/tests.py
@@ -0,0 +1,67 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("./chromedriver")
+        self.driver.implicitly_wait(20)
+        driver = self.driver
+        driver.get("http://www.roadtripr.fun")
+
+# Navigation
+    def test_about_page(self):
+        driver = self.driver
+        about_page = driver.find_element_by_xpath("//*[@id='nav-menu-container']/ul/li[2]/a")
+        about_page.click()
+        self.assertEqual("http://www.roadtripr.fun/about", driver.current_url)
+
+    def test_parks_page(self):
+        driver = self.driver
+        parks_page = driver.find_element_by_xpath("//*[@id='nav-menu-container']/ul/li[4]/a")
+        parks_page.click()
+        self.assertEqual("http://www.roadtripr.fun/parks", driver.current_url)
+
+    def test_cities_page(self):
+        driver = self.driver
+        cities_page = driver.find_element_by_xpath("//*[@id='nav-menu-container']/ul/li[3]/a")
+        cities_page.click()
+        self.assertEqual("http://www.roadtripr.fun/cities", driver.current_url)
+
+    def test_restaurants_page(self):
+        driver = self.driver
+        restaurants_page = driver.find_element_by_xpath("//*[@id='nav-menu-container']/ul/li[5]/a")
+        restaurants_page.click()
+        self.assertEqual("http://www.roadtripr.fun/restaurants", driver.current_url)
+
+#   Test Instances
+    def test_cities_instance(self):
+        driver = self.driver
+        cities_page = driver.find_element_by_link_text("Cities")
+        cities_page.click()
+        cities = driver.find_element_by_xpath("")
+        cities.click()
+        assert "Population" in driver.page_source
+        assert "Nearby Restaurants" in driver.page_source
+        assert "Nearby Parks" in driver.page_source
+
+# Navigation between instances
+    def test_navigation(self):
+        driver = self.driver
+        parks_page = driver.find_element_by_link_text("Parks")
+        parks_page.click()
+        park = driver.find_element_by_xpath("")
+        park.click()
+        cities = driver.find_element_by_xpath("")
+        cities.click()
+        assert "Population" in driver.page_source
+        assert "Nearby Restaurants" in driver.page_source
+        assert "Nearby Parks" in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
